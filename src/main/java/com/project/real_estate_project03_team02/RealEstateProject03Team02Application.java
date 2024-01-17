a7d26a00a0893948fbdd47cebb28608b7c49b7dc
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2024-01-17 07:21:39 +0300

    first commit

---
+++ src/main/java/com/project/real_estate_project03_team02/RealEstateProject03Team02Application.java
@@ -0,0 +1,13 @@
+package com.project.real_estate_project03_team02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateProject03Team02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateProject03Team02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/real_estate_project03_team02/RealEstateProject03Team02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.real_estate_project03_team02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateProject03Team02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
