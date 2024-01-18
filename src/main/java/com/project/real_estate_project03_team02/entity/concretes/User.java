759d14934bb95629660d627082063f889bb86f75
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2024-01-18 21:35:43 +0300

    User

---
+++ src/main/java/com/project/real_estate_project03_team02/entity/concretes/User.java
@@ -0,0 +1,15 @@
+package com.project.real_estate_project03_team02.entity.concretes;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+}
---
+++ src/main/java/com/project/real_estate_project03_team02/entity/enums/RoleType.java
@@ -0,0 +1,7 @@
+package com.project.real_estate_project03_team02.entity.enums;
+
+public enum RoleType {
+    ADMIN,
+    MANAGER,
+    CUSTOMER
+}
