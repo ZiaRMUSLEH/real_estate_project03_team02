0e7ab13edc5ada6269ee0bfee5f214adb2442eb0
IT <it@its-macbook-air.mynetworksettings.com>
2024-01-19 13:51:29 -0500

    committing updated CategoryPropertyValue

--- src/main/java/com/project/real_estate_project03_team02/entity/concretes/CategoryPropertyValue.java
+++ src/main/java/com/project/real_estate_project03_team02/entity/concretes/CategoryPropertyValue.java
@@ -1,11 +1,22 @@
 package com.project.real_estate_project03_team02.entity.concretes;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "category_property_value")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryPropertyValue {
-
+//has this went through?
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
