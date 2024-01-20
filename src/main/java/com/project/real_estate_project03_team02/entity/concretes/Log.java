b80d618071096274e401171b09c6e9882427f148
MerveKpln <hamuss001@gmail.com>
2024-01-20 18:14:17 +0400

    Log Entity

---
+++ src/main/java/com/project/real_estate_project03_team02/entity/concretes/Log.java
@@ -0,0 +1,38 @@
+package com.project.real_estate_project03_team02.entity.concretes;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="logs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String log;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private  User user_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Advert advert_id;
+
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime create_at;
+
+}
