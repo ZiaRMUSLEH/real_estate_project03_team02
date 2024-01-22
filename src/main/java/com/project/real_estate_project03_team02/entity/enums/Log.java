a6528d2f09c6a389d62c11160ecede5e10d9b226
MerveKpln <hamuss001@gmail.com>
2024-01-22 21:32:26 +0400

    Added Log Enum

---
+++ src/main/java/com/project/real_estate_project03_team02/entity/enums/Log.java
@@ -0,0 +1,6 @@
+package com.project.real_estate_project03_team02.entity.enums;
+
+public enum Log {
+    Created, Updated, Deleted, Declined,
+    Tour_Request_Created, Tour_Request_Accepted, Tour_Request_Declined, Tour_Request_Canceled
+}
