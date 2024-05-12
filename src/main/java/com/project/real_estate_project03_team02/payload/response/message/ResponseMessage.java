0503499dd49df0e30af06a586b3ff15111489b46
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2024-05-12 21:52:55 +0300

    TourRequest Controller class Tests

--- src/main/java/com/project/real_estate_project03_team02/payload/response/message/ResponseMessage.java
+++ src/main/java/com/project/real_estate_project03_team02/payload/response/message/ResponseMessage.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseMessage<T> {
